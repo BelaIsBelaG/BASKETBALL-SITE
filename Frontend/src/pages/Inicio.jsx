215ae397665db446f0b023919e0cfdda559f31b9
Isabela Gonzalez <159276237+BelaIsBelaG@users.noreply.github.com>
2025-05-29 04:58:23 +0000

    frontend/src/pages/Inicio.jsx

---
+++ Frontend/src/pages/Inicio.jsx
@@ -0,0 +1,11 @@
+// src/pages/Inicio.jsx
+import React from 'react';
+
+export default function Inicio() {
+  return (
+    <div className="container mt-5">
+      <h2>Bienvenido a Basket App</h2>
+      <p>Usa la barra de navegación para moverte entre las secciones.</p>
+    </div>
+  );
+}
